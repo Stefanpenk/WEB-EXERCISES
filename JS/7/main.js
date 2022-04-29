46772384f9d435d648df173db6251e30161d81b4
Stefan Penk <stefan.penk@gmail.com>
2022-04-29 14:38:07 +0200

    next event traning

--- JS/7/main.js
+++ JS/7/main.js
@@ -3,5 +3,19 @@ const h1 = document.querySelector('h1');
 document.body.addEventListener('mousemove', (e) => {
   // h1.textContent = `${e.clientX}, ${e.clientY}`;
   // h1.textContent = `${e.pageX}, ${e.pageY}`;
-  h1.textContent = `${e.screenX}, ${e.screenY}`;
+
+  const x = e.clientX;
+  const y = e.clientY;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  h1.textContent = `${x}, ${y}`;
+
+  const red = x / width * 100;
+  const green = y / height * 100;
+  const blue = ((x / width * 100) + (y / height * 100)) / 2;
+
+  document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`;
+
+
 })
