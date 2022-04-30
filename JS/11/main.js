260d04a78b267bf0397437bc710a5ba8161198da
Stefan Penk <stefan.penk@gmail.com>
2022-04-30 09:11:58 +0200

    multi passwords handler with forEach

--- JS/11/main.js
+++ JS/11/main.js
@@ -1,22 +1,39 @@
 const input = document.querySelector('input');
 const div = document.querySelector('div');
 
-const password = 'user';
-const message = 'I will be at work till 19:00 today.'
+const passwords = ['user', 'pass'];
+const messages = ['I will be at work till 19:00 today.', 'you should change your password.'];
 
+//SINGLE PASSWORD HANDLER
+// input.addEventListener('input', (e) => {
+//     if (password === e.target.value) {
+//         div.textContent = message;
+//         e.target.value = '';
+//     } else {
+//         div.textContent = '';
+//     }
+// });
+
+
+//MULTI PASSWORDS HANDLER
 input.addEventListener('input', (e) => {
-    if (password === e.target.value) {
-        div.textContent = message;
-        e.target.value = '';
-    } else {
-        div.textContent = '';
-    }
-})
+    div.textContent = " ";
+    const text = e.target.value;
+
+    passwords.forEach((password, index, array) => {
+        if (password === text) {
+            div.textContent = messages[index];
+            e.target.value = '';
+            console.log(array)
+        }
+    })
+
+});
 
 input.addEventListener('focus', (e) => {
     e.target.classList.add('active');
-})
+});
 
 input.addEventListener('blur', (e) => {
     e.target.classList.remove('active');
-})
+});
