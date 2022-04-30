8c6dc5156fb3de0be6dda851e8b8c17a75b3d5b4
Stefan Penk <stefan.penk@gmail.com>
2022-04-30 11:24:23 +0200

    code generator

--- JS/13/main.js
+++ JS/13/main.js
@@ -6,4 +6,12 @@ const names = ["John", "Kate", "Mike", "Anna", "Kira", "Marry", "Andrew", "Matt"
 const prefixs = ["I think that", "I am sure that", "Honestly, I think that", "That should be", "I am 100% hunder that it should be", "I am convinced that"];
 
 
+
+
+const nameGenerator = () => {
+    const index = Math.floor(Math.random() * names.length);
+    const prefixIndex = Math.floor(Math.random() * prefixs.length);
+    div.textContent = `${prefixs[prefixIndex]} ${names[index]}.`;
+}
+
 btn.addEventListener('click', nameGenerator)
