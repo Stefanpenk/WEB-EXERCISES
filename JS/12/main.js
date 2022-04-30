2860b4ff1a9e84ad7fad3200695330f49444309f
Stefan Penk <stefan.penk@gmail.com>
2022-04-30 09:51:35 +0200

    map() method exercise

---
+++ JS/12/main.js
@@ -0,0 +1,12 @@
+const input = document.querySelector("input");
+const passwords = ['oNe', 'tWo'];
+const messages = ["great", "works"]
+
+
+
+
+const showMessage = (e) => {
+
+}
+
+input.addEventListener("input", showMessage)
