b426e69f0934945ddf42a351c45514e5e83cbce1
Stefan Penk <stefan.penk@gmail.com>
2022-04-30 10:04:24 +0200

    map() + loop

--- JS/12/main.js
+++ JS/12/main.js
@@ -1,12 +1,19 @@
 const input = document.querySelector("input");
 const passwords = ['oNe', 'tWo'];
-const messages = ["great", "works"]
+const messages = ["great", "works"];
+const div = document.querySelector('div');
 
 
-
+const lowerCasePass = passwords.map(password => password.toLowerCase());
 
 const showMessage = (e) => {
+    const textInput = e.target.value.toLowerCase();
 
+    for (let i = 0; i < lowerCasePass.length; i++) {
+        if (textInput === lowerCasePass[i]) {
+            div.textContent = messages[i];
+        }
+    }
 }
 
 input.addEventListener("input", showMessage)
