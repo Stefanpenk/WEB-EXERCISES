d294dbcbe843293d8ed21513b56c8ecfdb20546e
Stefan Penk <stefan.penk@gmail.com>
2022-04-30 10:57:26 +0200

    math.random training

---
+++ JS/13/main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('button');
+const div = document.querySelector('div');
+
+const names = ["John", "Kate", "Mike", "Anna", "Kira", "Marry", "Andrew", "Matt"];
+
+const prefixs = ["I think that", "I am sure that", "Honestly, I think that", "That should be", "I am 100% hunder that it should be", "I am convinced that"];
+
+
+btn.addEventListener('click', nameGenerator)
