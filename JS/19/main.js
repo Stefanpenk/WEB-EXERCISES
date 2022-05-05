5a748479d2d1c3c0bd2e08071d34f31dea79dc83
Stefan Penk <stefan.penk@gmail.com>
2022-05-05 15:03:37 +0200

    added |

--- JS/19/main.js
+++ JS/19/main.js
@@ -1,12 +1,20 @@
 const spanText = document.querySelector('.text');
-const txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, a, ut natus accusantium dolore alias minima ratione hic laboriosam culpa ea maiores temporibus nulla cupiditate veniam impedit magni distinctio quod.'
+const spanCursor = document.querySelector('.cursor');
+const txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, a, ut natus accusantium dolore alias minima ratione hic laboriosam culpa ea maiores temporibus nulla cupiditate veniam impedit magni distinctio quod.';
 
 let indexText = 0;
-const time = 20;
+const time = 40;
 
 const addLetter = () => {
     spanText.textContent += txt[indexText];
     indexText++;
     if (indexText === txt.length) clearInterval(indexTyping);
-}
+};
+
+const cursorAnimation = () => {
+    spanCursor.classList.toggle('active');
+};
+
 const indexTyping = setInterval(addLetter, time);
+
+setInterval(cursorAnimation, 400);
